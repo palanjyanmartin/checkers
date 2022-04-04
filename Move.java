ce6f7bb3747b9853a7f13c9ee386dc8f65fd2239
8bitbrained <102755810+8bitbrained@users.noreply.github.com>
2022-04-04 22:08:16 +0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,26 +1,41 @@
 public class Move {
 	public Position initial=new Position();
 	public Position result=new Position();
-	/** constructor with two psoitions*/
+	/** 
+	*constructor with two positions
+	*@param ini,res:Initial and Result position type variables
+	*/
 	public Move(Position ini,Position res) {
 		this.initial=ini;
 		this.result=res;
 	}
-	/** copy constructor */
+	/** 
+	*copy constructor
+	*@param a: Move type variable
+	*/
 	public Move(Move a) {
 		this.initial=a.initial;
 		this.result=a.result;
 		
 	}
-	/** getter for initial position*/
+	/**
+	*getter for initial position
+	*@return Initial position:
+	*/
 	public Position getInitial() {
 		return this.initial;
 	}
-	/** getter for resulting position*/
+	/**
+	*getter for resulting position
+	*@return Result position:
+	*/
 	public Position getResult() {
 		return this.result;
 	}
-	/** string representation of the position*/
+	/** 
+	*string representation of the position
+	*@result String innterpretation of the given position e.g. [0,0]="A8":
+	*/
 	public String toString() {
 	 return initial.toString(this.initial) +" "+ result.toString(this.result);
 		
