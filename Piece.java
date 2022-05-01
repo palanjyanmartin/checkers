15350d2c801c3059d246ba24ec3e07f788171708
palanjyanmartin <101521311+palanjyanmartin@users.noreply.github.com>
2022-05-02 02:07:31 +0400

    Piece.java
    Abstract!

--- Piece.java
+++ Piece.java
@@ -1,41 +1,63 @@
 package am.aua.checkers.core;
 /**
- * @author Gor Hovakimyan <a href="mailto:gor_hovakimyan@edu.aua.am">gor_hovakimyan@edu.aua.am</a>
- * @version 1.1
- * @since 1.0
+ * @author Gor Hovakimyan
+* References Martin Palanjyan's HW09
  */
-public class Piece {
-	
-/** represent the color of piece */
-	private Checkers.PieceColor color;
-	
-	/**
-     * A no-arg constructor, initializes with WHITE .
-     */
-	public Piece() {
-	 this.color = color.WHITE;
-	}
-	
-	 /**
-     * A copy constructor, initializes according to another
-     * <code>Checkers.PieceColor</code> enum variable.
-     */
-	public Piece(Checkers.PieceColor color) {
-		this.color = color;
-	}
-	
+
+public abstract class Piece implements Cloneable {
+    private Chess.PieceColor color;
+
+    public Piece(Chess.PieceColor color) {
+        this.color = color;
+    }
+
+    public Piece() {
+        this(Chess.PieceColor.WHITE);
+    }
 
     /**
-     * Accessor method for the color of the piece.
+     * An abstract method
      *
-     * @return          the color of the piece
+     * @param chess
+     * @param p
+     * @return
      */
-	public Checkers.PieceColor getPiece() {
-		return this.color;
-	}
-	
-	/** In this moment it just returns null. */
-	public Position[] allDestinations(Checkers Checkers, Position p) {
-		return null;
-	}
+
+    public abstract Position[] allDestinations(Chess chess, Position p);
+
+    /**
+     * An accessor for the color that cannot be overriden.
+     *
+     * @return
+     */
+
+    public final Chess.PieceColor getPieceColor() {
+        return this.color;
+    }
+
+
+    public Piece clone() {
+        try {
+            Piece copy = (Piece) super.clone();
+            copy.color = color;
+            return copy;
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+
+
+    }
+
+    public boolean equals(Object otherObject) {
+        if (otherObject == null) {
+            return false;
+        } else if (getClass() != otherObject.getClass()) {
+            return false;
+        } else {
+            Piece otherPiece = (Piece) otherObject;
+            return (this.color.equals(otherPiece.getPieceColor()));
+        }
+
+
+    }
 }
