12a81ab8cf5245486ae4a4a0cfeb7d7cb6709185
8bitbrained <102755810+8bitbrained@users.noreply.github.com>
2022-05-02 16:10:42 +0400

    Update IlligalSymbolException.java

--- IlligalSymbolException.java
+++ IlligalSymbolException.java
@@ -1,6 +1,6 @@
 public class IllegalArrgunmentException extends Exception{
   public IllegalArrgunmentException(){
-    Super("Illigal arrangemnt for a Checkers game.");
+    Super("Illigal piece arrangement or character representation of a piece for a Checkers game.");
   }
   public IllegalArrgunmentException(String message){
     super(message);
