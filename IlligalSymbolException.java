c361fc07910ec4d71b618610193dd58a8201d5a7
8bitbrained <102755810+8bitbrained@users.noreply.github.com>
2022-05-02 16:03:09 +0400

    Create IlligalSymbolException.java

---
+++ IlligalSymbolException.java
@@ -0,0 +1,7 @@
+public class IllegalArrgunmentException extends Exception{
+  public IllegalArrgunmentException(){
+    Super("Illigal arrangemnt for a Checkers game.");
+  }
+  public IllegalArrgunmentException(String message){
+    super(message);
+  }
