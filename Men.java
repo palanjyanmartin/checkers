2c083fb8f5df787f773d5a17c5643a73c2adff14
palanjyanmartin <101521311+palanjyanmartin@users.noreply.github.com>
2022-05-04 02:52:36 +0400

    Men.java
    Update

--- Men.java
+++ Men.java
@@ -2,6 +2,7 @@ package am.aua.checkers.core;
 
 /**
  * The <code>am.aua.Checkers.core.Men</code> class contains the behaviour of Men pieces.
+ *
  * @author Martin Palanjyan
  * References Martin Palanjyan HW08
  */
@@ -50,48 +51,52 @@ public class Men extends Piece {
      * that a Men can move into from position <code>p</code>
      */
     public Position[] allDestinations(Checkers checkers, Position p) {
+        Checkers.PieceColor myColor = checkers.getPieceAt(p).getPieceColor();
         int[] rankOfWhites = {-1, -1};
         int[] fileOfWhites = {1, -1};
         int[] rankOfBlacks = {1, 1};
         int[] fileOfBlacks = {1, -1};
-        if (checkers.getTurn() == Checkers.PieceColor.WHITE) {
-            Position[] result = new Position[0];
-            Position current = Position.generateFromRankAndFile(p.getRank() - rankOfWhites[0], p.getPosition() - fileOfWhites[0]);
-            Position current2 = Position.generateFromRankAndFile(p.getRank() - rankOfWhites[1], p.getPosition() - fileOfWhites[1]);
+        Position[] result = new Position[0];
+        Position current;
+        Position current2;
+        if (myColor == Checkers.PieceColor.WHITE) {
+            //System.out.println("1");
+            current = Position.generateFromRankAndFile(p.getRank() - rankOfWhites[0], p.getPosition() + fileOfWhites[0]);
+            //System.out.println("2");
+            current2 = Position.generateFromRankAndFile(p.getRank() - rankOfWhites[1], p.getPosition() + fileOfWhites[1]);
 
             if ((current != null) && ((checkers.isEmpty(current)) || (checkers.getPieceAt(p).getPieceColor() != checkers.getPieceAt(current).getPieceColor()))) {
                 result = Position.appendPositionsToArray(result, current);
             }
 
-            if (current2 != null && (checkers.getPieceAt(current2) != null) && (checkers.getPieceAt(p).getPieceColor() != checkers.getPieceAt(current2).getPieceColor())) {
+            if ((current2 != null) && ((checkers.isEmpty(current2)) || (checkers.getPieceAt(p).getPieceColor() != checkers.getPieceAt(current2).getPieceColor()))) {
                 result = Position.appendPositionsToArray(result, current2);
             }
 
             if (p.getRank() == 8) {
-                Kings.reachablePositions(checkers,p);
+                Kings.reachablePositions(checkers, p);
             }
-            return result;
-
-        } else if (checkers.getTurn() == Checkers.PieceColor.BLACK) {
-            Position[] result = new Position[0];
-            Position current = Position.generateFromRankAndFile(p.getRank() - rankOfBlacks[0], p.getPosition() - fileOfBlacks[0]);
-            Position current2 = Position.generateFromRankAndFile(p.getRank() - rankOfBlacks[1], p.getPosition() - fileOfBlacks[1]);
 
 
-            if ((current != null) && (checkers.getPieceAt(current) != null) && (checkers.getPieceAt(p).getPieceColor() != checkers.getPieceAt(current).getPieceColor())) {
+        } else {
+            current = Position.generateFromRankAndFile(p.getRank() - rankOfBlacks[0], p.getPosition() - fileOfBlacks[0]);
+            current2 = Position.generateFromRankAndFile(p.getRank() - rankOfBlacks[1], p.getPosition() - fileOfBlacks[1]);
+
+
+            if ((current != null) && ((checkers.isEmpty(current)) || (checkers.getPieceAt(p).getPieceColor() != checkers.getPieceAt(current).getPieceColor()))) {
                 result = Position.appendPositionsToArray(result, current);
             }
 
-            if (current2 != null && (checkers.getPieceAt(current2) != null) && (checkers.getPieceAt(p).getPieceColor() != checkers.getPieceAt(current2).getPieceColor())) {
+            if ((current2 != null) && ((checkers.isEmpty(current2)) || (checkers.getPieceAt(p).getPieceColor() != checkers.getPieceAt(current2).getPieceColor()))) {
                 result = Position.appendPositionsToArray(result, current2);
             }
-            if (p.getRank() == 0){
-                Kings.reachablePositions(checkers,p);
+
+            if (p.getRank() == 0) {
+                Kings.reachablePositions(checkers, p);
             }
 
-            return result;
-
         }
-        return new Position[0];
+        return result;
     }
+
 }
