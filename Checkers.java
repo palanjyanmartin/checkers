8121947582a2839c2025b9f391fa48bd99accc88
palanjyanmartin <101521311+palanjyanmartin@users.noreply.github.com>
2022-05-09 14:23:22 +0400

    Checkers.java

--- Checkers.java
+++ Checkers.java
@@ -1,14 +1,16 @@
 package am.aua.checkers.core;
 
+import java.util.ArrayList;
+
 /**
  * The <code>am.aua.checkers.core.checkers</code> class encapsulates the state of an ongoing game of
  * checkers.
  *
- * @authors Martin Palanjyan, Gor Hovakimyan
+ * @author Martin Palanjyan
  * References Martin Palanjyan's HW08
  */
 public class Checkers implements Cloneable {
-    
+
     //mutable, e.g. performMove modifies state
     /**
      * The number of board ranks in checkers.
@@ -37,7 +39,15 @@ public class Checkers implements Cloneable {
      * which indicates which player is about to make a move.
      */
     public Checkers() throws IllegalArrangementException {
-       " M M M MM M M M  M M M M                m m m m  m m m mm m m m "
+        this("-m-m-m-m" +
+                "m-m-m-m-" +
+                "-m-m-m-m" +
+                "--------" +
+                "--------" +
+                "M-M-M-M-" +
+                "-M-M-M-M" +
+                "M-M-M-M-",
+                PieceColor.WHITE);
     }
 
 
@@ -63,20 +73,23 @@ public class Checkers implements Cloneable {
                     this.board[i][j] = new Men(PieceColor.BLACK);
                     index++;
                 } else if (represent.charAt(index) == 'K') {
-                    this.board[i][j] = new Kings(PieceColor.WHITE, true);
+                    this.board[i][j] = new Kings(PieceColor.WHITE);
                     index++;
                 } else if (represent.charAt(index) == 'k') {
-                    this.board[i][j] = new Kings(PieceColor.BLACK, true);
+                    this.board[i][j] = new Kings(PieceColor.BLACK);
+                }else if (represent.charAt(index) == '-'){
+                    this.board[i][j] = null;
+                    index++;
                 }
             }
             this.color = pieceColor;
         }
 
     }
-    
+
     /**
-    * Creats the clone of type Checkers.
-    */
+     * Creats the clone of type Checkers.
+     */
     public Checkers clone() {
         try {
             Checkers copy = (Checkers) super.clone();
@@ -86,122 +99,123 @@ public class Checkers implements Cloneable {
             return null;
         }
     }
-        /**
-         * Returns a <code>Piece</code> matrix representing the game board and the
-         * pieces on it.
-         *
-         * @return the board as a <code>Piece</code> matrix
-         */
+    /**
+     * Returns a <code>Piece</code> matrix representing the game board and the
+     * pieces on it.
+     *
+     * @return the board as a <code>Piece</code> matrix
+     */
 
-        public Piece[][] getBoard() {
-            //returns deep copy
-            Piece[][] board = new Piece[BOARD_RANKS][BOARD_FILES];
-            for (int i = 0; i < BOARD_RANKS; i++)
-                for (int j = 0; j < BOARD_FILES; j++)
-                    board[i][j] = (Piece) this.board[i][j].clone();
-            return board;
-        }
-
-        /**
-         * Returns color which indicates white's or black's turn, respectively.
-         *
-         * @return color
-         */
-        public Checkers.PieceColor getTurn () {
-            return color;
-        }
-
-        /**
-         * Checks if the game is over or if it still continues.
-         * <p>
-         * Current version always returns <code>false</code>. The functionality will
-         * be fixed in future versions.
-         *
-         * @return true if and only if the game is over
-         */
-        public boolean isGameOver () {
-            return false;
-        }
-
-
-        /**
-         * Checks if a given position on the board is empty.
-         *
-         * @param p the position that should be tested
-         * @return true if and only if <code>p</code> is null
-         */
-        public boolean isEmpty(Position p){
-            return this.board[p.getRank()][p.getPosition()] == null;
-        }
-
-      
-        /**
-         * Returns the <code>Piece</code> representing the piece on a given position
-         * on the board.
-         *
-         * @param p the position on the board
-         * @return the <code>Piece</code> representing the piece on
-         * <code>p</code>
-         */
-        public Piece getPieceAt (Position p){
-            return this.board[p.getRank()][p.getPosition()];
-        }
-
-        /**
-         * Identifies and returns the set of all positions that can serve as
-         * destinations for a move from the given origin.
-         * <p>
-         * If the origin is not a real object or is empty, the method just returns
-         * <code>null</code>. Otherwise, it returns a listing of all eligible
-         * destinations based on the type of piece in origin
-         *
-         * @param origin the origin position on the board
-         * @return the array with the set of reachable positions
-         */
-        public Position[] reachableFrom (Position origin){
-            if (origin == null || this.isEmpty(origin)) {
-                return null;
-            } else
-                return getPieceAt(origin).allDestinations(this, origin);
-        }
-
-
-        /**
-         * Attempts to make a given <code>am.aua.checkers.core.Move</code>. If the move is valid, i.e.
-         * the destination is in the set reachable from the origin, the method
-         * correctly updates the state of <code>am.aua.checkers.core.checkers</code> and returns
-         * <code>true</code>. Otherwise, the method returns <code>false</code>.
-         *
-         * @param m the move attempted
-         * @return true if and only if the move was successfully made
-         */
-        public boolean performMove (Move m){
-            Position origin = m.getOrigin();
-            Position destination = m.getDestination();
-            Position[] reachable = this.reachableFrom(origin);
-            if (reachable != null) {
-                for (int i = 0; i < reachable.length; i++)
-                    if (destination.getRank() == reachable[i].getRank()
-                            && destination.getPosition() == reachable[i].getPosition()) {
-                        this.board[destination.getRank()][destination.getPosition()] =
-                                this.board[origin.getRank()][origin.getPosition()];
-                        this.board[origin.getRank()][origin.getPosition()] = null;
-                        if (this.board[destination.getRank()][destination.getPosition()].getPieceColor() == PieceColor.WHITE) {
-                            color = PieceColor.BLACK;
-                            return true;
-                        } else if (this.board[destination.getRank()][destination.getPosition()].getPieceColor() == PieceColor.BLACK) {
-                            color = PieceColor.WHITE;
-                            return true;
-                        }
-                        return false;
-                    }
-                return false;
-            }
-            return false;
-        }
+    public Piece[][] getBoard() {
+        //returns deep copy
+        Piece[][] board = new Piece[BOARD_RANKS][BOARD_FILES];
+        for (int i = 0; i < BOARD_RANKS; i++)
+            for (int j = 0; j < BOARD_FILES; j++)
+                board[i][j] = (Piece) this.board[i][j].clone();
+        return board;
     }
 
 
+
+    /**
+     * Returns color which indicates white's or black's turn, respectively.
+     *
+     * @return color
+     */
+    public Checkers.PieceColor getTurn () {
+        return color;
+    }
+
+    /**
+     * Checks if the game is over or if it still continues.
+     * <p>
+     * Current version always returns <code>false</code>. The functionality will
+     * be fixed in future versions.
+     *
+     * @return true if and only if the game is over
+     */
+    public boolean isGameOver () {
+        return false;
+    }
+
+
+    /**
+     * Checks if a given position on the board is empty.
+     *
+     * @param p the position that should be tested
+     * @return true if and only if <code>p</code> is null
+     */
+    public boolean isEmpty(Position p){
+        return this.board[p.getRank()][p.getPosition()] == null;
+    }
+
+
+    /**
+     * Returns the <code>Piece</code> representing the piece on a given position
+     * on the board.
+     *
+     * @param p the position on the board
+     * @return the <code>Piece</code> representing the piece on
+     * <code>p</code>
+     */
+    public Piece getPieceAt (Position p){
+        return this.board[p.getRank()][p.getPosition()];
+    }
+
+    /**
+     * Identifies and returns the set of all positions that can serve as
+     * destinations for a move from the given origin.
+     * <p>
+     * If the origin is not a real object or is empty, the method just returns
+     * <code>null</code>. Otherwise, it returns a listing of all eligible
+     * destinations based on the type of piece in origin
+     *
+     * @param origin the origin position on the board
+     * @return the array with the set of reachable positions
+     */
+    public ArrayList<Position> reachableFrom(Position origin) {
+        if (origin == null || this.isEmpty(origin))
+            return null;
+
+        return board[origin.getRank()][origin.getPosition()].allDestinations(this,
+                origin);
+    }
+
+
+    /**
+     * Attempts to make a given <code>am.aua.checkers.core.Move</code>. If the move is valid, i.e.
+     * the destination is in the set reachable from the origin, the method
+     * correctly updates the state of <code>am.aua.checkers.core.checkers</code> and returns
+     * <code>true</code>. Otherwise, the method returns <code>false</code>.
+     *
+     * @param m the move attempted
+     * @return true if and only if the move was successfully made
+     */
+    public boolean performMove (Move m){
+        Position origin = m.getOrigin();
+        Position destination = m.getDestination();
+        ArrayList<Position> reachable = this.reachableFrom(origin);
+        if (reachable != null) {
+            for (Position position : reachable)
+                if (destination.getRank() == position.getRank()
+                        && destination.getPosition() == position.getPosition()) {
+                    this.board[destination.getRank()][destination.getPosition()] =
+                            this.board[origin.getRank()][origin.getPosition()];
+                    this.board[origin.getRank()][origin.getPosition()] = null;
+                    if (this.board[destination.getRank()][destination.getPosition()].getPieceColor() == PieceColor.WHITE) {
+                        color = PieceColor.BLACK;
+                        return true;
+                    } else if (this.board[destination.getRank()][destination.getPosition()].getPieceColor() == PieceColor.BLACK) {
+                        color = PieceColor.WHITE;
+                        return true;
+                    }
+                    return false;
+                }
+            return false;
+        }
+        return false;
+    }
+}
 
 
 
