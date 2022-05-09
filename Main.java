bca6836331a1cec08a174bed8712d17429095a9d
palanjyanmartin <101521311+palanjyanmartin@users.noreply.github.com>
2022-05-09 14:26:33 +0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
+package am.aua.checkers;
 
+import am.aua.checkers.cli.*;
+import am.aua.checkers.core.IllegalArrangementException;
+
+
+public class Main {
+    public static void main(String[] args) throws IllegalArrangementException {
+
+        CheckersConsole checkersConsole = new CheckersConsole();
+        checkersConsole.play();
+    }
+}
