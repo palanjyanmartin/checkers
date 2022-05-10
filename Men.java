983f790f7de8b6c19b99405d28a5697b477c35a8
gorhovakimyan200121 <102758761+gorhovakimyan200121@users.noreply.github.com>
2022-05-10 17:38:11 +0400

    Update Men.java
    qceci

--- Men.java
+++ Men.java
@@ -101,5 +101,68 @@ public class Men extends Piece {
         }
         return result;
     }
+    
+     private ArrayList<Position> result = new ArrayList<Position>();
+    
+    public ArrayList<Position> allDestinations(Checkers checkers, Position p) {
+        ArrayList<Position> result = new ArrayList<>();
+        Checkers.PieceColor myColor = checkers.getPieceAt(p).getPieceColor();
+
+        Position left = null;
+        Position right = null;
+        Position leftEat = null;
+        Position rightEat=null;
+        Position leftBackEat=null;
+        Position rightBackEat=null;
+        
+
+        if (myColor == Checkers.PieceColor.WHITE) {
+            left = Position.generateFromRankAndFile(p.getRank() - 1, p.getPosition() - 1);
+            right = Position.generateFromRankAndFile(p.getRank() - 1, p.getPosition() + 1);
+            leftEat= Position.generateFromRankAndFile(left.getRank()-1, left.getPosition()-1);
+            rightEat= Position.generateFromRankAndFile(right.getRank()-1, right.getPosition()+1);
+            leftBackEat=Position.generateFromRankAndFile(left.getRank()-1, left.getPosition()-1);
+            rightBackEat= Position.generateFromRankAndFile(left.getRank()-1, left.getPosition()-1);
+        } else {
+            left = Position.generateFromRankAndFile(p.getRank() + 1, p.getPosition() - 1);
+            right = Position.generateFromRankAndFile(p.getRank() + 1, p.getPosition() + 1);
+        }
+        if (left != null && left.getRank() >= 0 && left.getRank() <= 7 && left.getPosition() >= 0 && left.getPosition() <= 7 && checkers.isEmpty(left)) {
+            result.add(left);
+        }
+        if (right != null && right.getRank() >= 0 && right.getRank() <= 7 && right.getPosition() >= 0 && right.getPosition() <= 7 && checkers.isEmpty(right)) {
+            result.add(right);
+        }
+
+
+        return result;
+    }
+
+	public boolean eatable(Checkers checkers, Position p) {
+			
+		
+			if (checkers.getPieceAt(new Position((p.getRank() + 1), p.getPosition() + 1)).getPieceColor() != checkers.getPieceAt(p).getPieceColor()
+                 && checkers.isEmpty(new Position((p.getRank() + 2), p.getPosition() + 2))
+                  && p.getRank() + 2 <= 7 && p.getPosition() + 2 <= 7) 
+              return true;
+			
+          
+          if (checkers.getPieceAt(new Position((p.getRank() + 1), p.getPosition() - 1)).getPieceColor() != checkers.getPieceAt(p).getPieceColor()
+                  && checkers.isEmpty(new Position((p.getRank() + 2), p.getPosition() - 2))
+                  && p.getRank() + 2 <= 7 && p.getPosition() - 2 >= 0) 
+              return true;
+          
+          if (checkers.getPieceAt(new Position((p.getRank() - 1), p.getPosition() + 1)).getPieceColor() != checkers.getPieceAt(p).getPieceColor()
+                  && checkers.isEmpty(new Position((p.getRank() - 2), p.getPosition() + 2))
+                  && p.getRank() - 2 >= 0 && p.getPosition() + 2 <= 7) 
+              return true;
+          
+          if (checkers.getPieceAt(new Position((p.getRank() - 1), p.getPosition() - 1)).getPieceColor() != checkers.getPieceAt(p).getPieceColor()
+                  && checkers.isEmpty(new Position((p.getRank() - 2), p.getPosition() - 2))
+                  && p.getRank() - 2 >= 0 && p.getPosition() - 2 >= 0) 
+              return true;
+          return false;
+		}
+
 
 }
