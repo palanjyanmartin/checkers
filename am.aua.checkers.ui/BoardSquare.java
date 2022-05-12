03919f420ca7a408cc636a124129ae2187e7ca4e
palanjyanmartin <101521311+palanjyanmartin@users.noreply.github.com>
2022-05-13 02:01:11 +0400

    am.aua.checkers.ui/BoardSquare.java
    Final!

--- am.aua.checkers.ui/BoardSquare.java
+++ am.aua.checkers.ui/BoardSquare.java
@@ -2,12 +2,29 @@ package am.aua.checkers.ui;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * A class that provides created Board Square to be used in CheckersUI class
+ * <code>am.aua.checkers.ui.BoardSquare</code>.
+ * @author Martin Palanjyan
+ * @author Arman Khachatryan
+ * @author Gor Hovakimyan
+ * References
+ * Martin Palanjyan's HW10
+ */
+
 public class BoardSquare extends JButton {
     public static final Color light = Color.WHITE;
     public static final Color dark = Color.GRAY;
     private int xCord;
     private int yCord;
     private Color color;
+
+    /**
+     * A constructor
+     * @param color
+     * @param xCord
+     * @param yCord
+     */
 
     public BoardSquare(boolean color, int xCord, int yCord) {
         super();
@@ -23,6 +40,11 @@ public class BoardSquare extends JButton {
 
     }
 
+    /**
+     * Method that creates an array of coordinates.
+     * @return
+     */
+
     public int[] getCoordinate() {
         int[] newArray = new int[2];
         newArray[0] = this.xCord;
@@ -30,6 +52,10 @@ public class BoardSquare extends JButton {
         return newArray;
     }
 
+    /**
+     * A method that will set icons of the pieces.
+     * @param letter
+     */
 
     public void setPiece(String letter) {
         switch (letter) {
@@ -53,9 +79,18 @@ public class BoardSquare extends JButton {
         }
     }
 
+    /**
+     * A method that set the icon of the piece to empty one.
+     */
+
     public void setPiece() {
         this.setIcon(null);
     }
+
+    /**
+     * A method that highlights the square to red or keep it the same.
+     * @param highlighted
+     */
 
     public void setHighlight(boolean highlighted) {
         if (highlighted) {
