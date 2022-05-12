0c610f911e548e03e7acea5ae619a7fb392cdc61
palanjyanmartin <101521311+palanjyanmartin@users.noreply.github.com>
2022-05-12 10:29:24 +0400

    CheckersUI.java

--- CheckersUI.java
+++ CheckersUI.java
@@ -12,12 +12,16 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class CheckersUI extends JFrame implements ActionListener {
+    private boolean flag;
+    private boolean isHighlighted;
+    private boolean success2;
     private Checkers game;
     private JPanel titlePanel = new JPanel();
     private JPanel buttonPanel = new JPanel();
     private JLabel textField = new JLabel();
     private BoardSquare[][] buttons = new BoardSquare[Checkers.BOARD_RANKS][Checkers.BOARD_FILES];
     private Position position;
+    private Position origin;
     private Position destination;
 
     public static final int Width = 1000;
@@ -29,8 +33,16 @@ public class CheckersUI extends JFrame implements ActionListener {
         setSize(Width, Height);
         getContentPane().setBackground(Color.WHITE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        setLayout(new FlowLayout());//
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.setBounds(0, 0, 100, 300);
+        textField.setBackground(Color.BLACK);
+        textField.setForeground(Color.ORANGE);
+        textField.setFont(new Font("Serif", Font.BOLD, 50));
+        textField.setHorizontalAlignment(JLabel.CENTER);
+        textField.setText("Checkers");
         buttonPanel.setLayout(new GridLayout(Checkers.BOARD_RANKS, Checkers.BOARD_FILES));
+        //buttonPanel.setBackground(Color.BLACK);
         for (int i = 0; i < Checkers.BOARD_RANKS; i++) {
             for (int j = 0; j < Checkers.BOARD_FILES; j++) {
                 if ((i + j) % 2 == 0) {
@@ -38,7 +50,8 @@ public class CheckersUI extends JFrame implements ActionListener {
                 } else {
                     buttons[i][j] = new BoardSquare(false, i, j);
                 }
-                buttons[i][j].setPreferredSize(new Dimension(95, 95));
+
+                buttons[i][j].setPreferredSize(new Dimension(90, 90));
                 buttonPanel.add(buttons[i][j]);
                 this.game = new Checkers();
                 if (game.getBoard()[i][j] != null) {
@@ -48,74 +61,101 @@ public class CheckersUI extends JFrame implements ActionListener {
                 buttons[i][j].addActionListener(e -> {
                     BoardSquare button = (BoardSquare) e.getSource();
                     boardClicked(button.getCoordinate());
-                    updatePieces();
+                    // updatePieces();
                 });
                 buttonPanel.add(buttons[i][j]);
             }
         }
 
+        titlePanel.add(textField);
         buttonPanel.setVisible(true);
+        add(titlePanel, BorderLayout.NORTH);
         add(buttonPanel, BorderLayout.CENTER);
     }
 
     private void boardClicked(int[] coordinates) {
         this.position = new Position(coordinates[0], coordinates[1]);
-        if (game.isEmpty(this.position)) {
-            BoardSquare boardSquare = buttons[position.getRank()][position.getPosition()];
-            boardSquare.setHighlight(false);
+        ArrayList<Position> positions = new ArrayList<>();
+        positions.add(0, this.position);
 
+        if (!flag){
+            System.out.println(positions.get(0).toString());
+        }
 
-        } else if (!game.isEmpty(this.position)) {
-            for (int j = 0; j < Checkers.BOARD_RANKS; j++) {
-                for (int k = 0; k < Checkers.BOARD_FILES; k++) {
-                    BoardSquare boardSquare = buttons[j][k];
+        if (!flag) {
+            if (game.getPieceAt(this.position) != null) {
+                if (game.isEmpty(this.position)) {
+                    BoardSquare boardSquare = buttons[position.getRank()][position.getPosition()];
                     boardSquare.setHighlight(false);
-                }
-
-            }
-
-            if ((game.getPieceAt(new Position(coordinates[0], coordinates[1])) != null) && (game.getTurn() == game.getPieceAt(new Position(coordinates[0], coordinates[1])).getPieceColor())) {
-                if (game.reachableFrom(new Position(coordinates[0], coordinates[1])) != null) {
-                    for (int i = 0; i < game.reachableFrom(new Position(coordinates[0], coordinates[1])).size(); i++) {
-                        this.destination = game.reachableFrom(new Position(coordinates[0], coordinates[1])).get(i);
-                        BoardSquare boardSquare = buttons[this.destination.getRank()][this.destination.getPosition()];
-                        boardSquare.setHighlight(true);
-                        updatePieces();
-                    }
-
-                }
-
-
-            }
-            ArrayList<Position> list = game.reachableFrom(new Position(coordinates[0], coordinates[1]));
-            if (list != null) {
-                for (Position value : list) {
-                    this.destination = value;
-                    if (!game.performMove(new Move(this.position, this.destination))) {
-                        System.out.println("Invalid position. Please try again.");
-
-                    }
-                }
-            }
-
-
-        }
-    }
-
-    private void updatePieces() {
-        for (int i = 0; i < Checkers.BOARD_RANKS; i++) {
-            for (int j = 0; j < Checkers.BOARD_FILES; j++) {
-                if (game.isEmpty(new Position(i, j))) {
-                    buttons[i][j].setPiece();
                 } else {
-                    buttons[i][j].setPiece(game.getPieceAt(new Position(i, j)).toString());
+                    for (int j = 0; j < Checkers.BOARD_RANKS; j++) {
+                        for (int k = 0; k < Checkers.BOARD_FILES; k++) {
+                            BoardSquare boardSquare = buttons[j][k];
+                            boardSquare.setHighlight(false);
+                        }
+                    }
+                }
+
+                ArrayList<Position> reachableSquares = game.reachableFrom(this.position);
+                ArrayList<Position> eatableSquares = game.eatableFrom(this.position);
+
+                for (int i = 0; i < Checkers.BOARD_RANKS; i++) {
+                    for (int j = 0; j < Checkers.BOARD_FILES; j++) {
+
+                        if (this.position != null &&
+                                this.position.getRank() == i && this.position.getPosition() == j)
+                            isHighlighted = true;
+                        for (int k = 0; reachableSquares != null &&
+                                k < reachableSquares.size(); k++)
+                            if (reachableSquares.get(k).getRank() == i &&
+                                    reachableSquares.get(k).getPosition() == j) {
+                                buttons[i][j].setHighlight(true);
+                                flag = true;
+                                break;
+                            }
+
+                        for (int k = 0; eatableSquares != null &&
+                                k < eatableSquares.size(); k++)
+                            if (eatableSquares.get(k).getRank() == i &&
+                                    eatableSquares.get(k).getPosition() == j) {
+                                buttons[i][j].setHighlight(true);
+                                flag = true;
+                                break;
+                            }
+                    }
                 }
             }
-
+        } else {
+            System.out.println(positions.get(0).toString());
+//                this.position = new Position(coordinates[0], coordinates[1]);
+//                positions.add(1,this.position);
+//                success2 = game.performMove(new Move(positions.get(0), positions.get(1)));
+//                    if (!success2) {
+//                        System.out.println("Invalid");
+//                    }
+//                    flag = false;
+//
+//
+//                }
         }
     }
 
 
-    public void actionPerformed(ActionEvent e) {
+        private void updatePieces () {
+            for (int i = 0; i < Checkers.BOARD_RANKS; i++) {
+                for (int j = 0; j < Checkers.BOARD_FILES; j++) {
+                    if (game.isEmpty(new Position(i, j))) {
+                        buttons[i][j].setPiece();
+                    } else {
+                        buttons[i][j].setPiece(game.getPieceAt(new Position(i, j)).toString());
+                    }
+                }
+
+            }
+        }
+
+
+        public void actionPerformed (ActionEvent e){
+        }
     }
-}
+
